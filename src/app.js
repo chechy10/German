2665b8ef42f4b1d8295a21e4b291d05624556ba8
hideonbush1010 <10rivenmain@gmail.com>
2024-10-01 23:26:18 -0500

    tree commit

--- src/app.js
+++ src/app.js
@@ -6,85 +6,22 @@ import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 const PORT = process.env.PORT ?? 3006
 
 /*********************FLUJOS IMPORTADOS*********************** */
-
 import { flowWelcome } from './flows/bienvenida.js'
-
 //-----------------------------------------------------------
-
 import { flowAboutus } from './flows/fmenu/aboutus.js'
-
 import { flowMenu } from './flows/fmenu/menu.js'
-
 //----------------------------------------------------------
-
 import { flowInitPedido } from './flows/fmenu/smenu/iniciarpedido.js'
 import { flowOrder } from './flows/fmenu/smenu/orden.js'
 import { flowpedidofin } from './flows/fmenu/smenu/pedidofin.js'
-
 //--------------------------------------------------------------
-
 import { flowDomicilio } from './flows/fmenu/smenu/otrasopciones/domicilio.js'
-
 //-------------------------------------------------------------------------------
-
 import { flowHogar } from './flows/fmenu/smenu/otrasopciones/domicilio/hogar.js'
 import { flowMetod } from './flows/fmenu/smenu/otrasopciones/transacciones/metodopago.js'
 import { flowUbicacion } from './flows/fmenu/smenu/otrasopciones/domicilio/ubicacion.js'
 
 /************************************************************* */
-
-/*const discordFlow = addKeyword('doc').addAnswer(
-    ['You can see the documentation here', '📄 https://builderbot.app/docs \n', 'Do you want to continue? *yes*'].join(
-        '\n'
-    ),
-    { capture: true },
-    async (ctx, { gotoFlow, flowDynamic }) => {
-        if (ctx.body.toLocaleLowerCase().includes('yes')) {
-            return gotoFlow(registerFlow)
-        }
-        await flowDynamic('Thanks!')
-        return
-    }
-)
-
-const welcomeFlow = addKeyword(['hi', 'hello', 'hola'])
-    .addAnswer(`🙌 Hello welcome to this *Chatbot*`)
-    .addAnswer(
-        [
-            'I share with you the following links of interest about the project',
-            '👉 *doc* to view the documentation',
-        ].join('\n'),
-        { delay: 800, capture: true },
-        async (ctx, { fallBack }) => {
-            if (!ctx.body.toLocaleLowerCase().includes('doc')) {
-                return fallBack('You should type *doc*')
-            }
-            return
-        },
-        [discordFlow]
-    )
-
-const registerFlow = addKeyword(utils.setEvent('REGISTER_FLOW'))
-    .addAnswer(`What is your name?`, { capture: true }, async (ctx, { state }) => {
-        await state.update({ name: ctx.body })
-    })
-    .addAnswer('What is your age?', { capture: true }, async (ctx, { state }) => {
-        await state.update({ age: ctx.body })
-    })
-    .addAction(async (_, { flowDynamic, state }) => {
-        await flowDynamic(`${state.get('name')}, thanks for your information!: Your age: ${state.get('age')}`)
-    })
-
-const fullSamplesFlow = addKeyword(['samples', utils.setEvent('SAMPLES')])
-    .addAnswer(`💪 I'll send you a lot files...`)
-    .addAnswer(`Send image from Local`, { media: join(process.cwd(), 'assets', 'sample.png') })
-    .addAnswer(`Send video from URL`, {
-        media: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTJ0ZGdjd2syeXAwMjQ4aWdkcW04OWlqcXI3Ynh1ODkwZ25zZWZ1dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LCohAb657pSdHv0Q5h/giphy.mp4',
-    })
-    .addAnswer(`Send audio from URL`, { media: 'https://cdn.freesound.org/previews/728/728142_11861866-lq.mp3' })
-    .addAnswer(`Send file from URL`, {
-        media: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
-    })*/
 
 const main = async () => {
     const adapterFlow = createFlow([
@@ -150,7 +87,9 @@ const main = async () => {
         })
     )
 
-    httpServer(+port) // Aquí usamos la variable 'port' en lugar de 'PORT'
+    // Cambia 'port' a 'PORT'
+    httpServer(PORT) // Asegúrate de usar 'PORT' aquí
 }
 
 main()
+
